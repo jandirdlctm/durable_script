5c02a0f02cc81cbc3f64484e7f1cb1eb789e4dc4
Jandir Porta <jandirporta@Jandirs-MacBook-Pro.local>
2024-12-23 01:51:09 -0700

    testing if div exists to change the functionality

--- app.js
+++ app.js
@@ -2,12 +2,11 @@
 
 // alert("I HAVE ARRIVED");
 
-const element = document.querySelector("#page-block-0 > section > div > div > div.relative.z-10.container.mx-auto.px-5.md\\:px-6.transition-all.duration-300.pt-20.lg\\:pt-48.pb-20.lg\\:pb-48 > div > div > div.flex.md\\:inline-flex.flex-col.md\\:flex-row.gap-4.w-full.md\\:w-max.mt-4");
-
-if (element) {
+const new_element = document.querySelector("#\\36 7668b4cb39ca82afeff4e37 > div > div.relative.z-10.container.mx-auto.px-5.md\\:px-6.transition-all.duration-300.pt-20.lg\\:pt-48.pb-20.lg\\:pb-48 > div > div > div.flex.md\\:inline-flex.flex-col.md\\:flex-row.gap-4.w-full.md\\:w-max.mt-4")
+if (new_element) {
   // Element was found
   console.log("Element exists! We can manipulate it safely.");
-  element.style.backgroundColor = "lightgreen";
+  new_element.style.backgroundColor = "lightgreen";
 } else {
   // Element does not exist
   console.warn("Element not found. Check your selector or ensure the element is in the DOM.");
