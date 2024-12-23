6a936aa58e2800cc4cb8c4585b6d718c62bffb63
Jandir Porta <jandirporta@Jandirs-MacBook-Pro.local>
2024-12-23 02:26:35 -0700

    added functionality to change the text of blog to events

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ if (new_element) {
   // Element does not exist
   console.warn("Element not found. Check your selector or ensure the element is in the DOM.");
 }
+
+const element_blog = document.querySelector("#page-block-0 > section > div.relative.z-10.container.mx-auto.px-5.md\\:px-6.py-12.lg\\:py-14.xl\\:py-20 > div.flex.flex-col.md\\:flex-row.md\\:items-center.md\\:justify-between.gap-3.lg\\:gap-4.mb-4.lg\\:mb-6 > h2")
+
+if (element_blog) {
+    // Element was found
+    console.log("Element exists! We can manipulate it safely.");
+  //   new_element.style.backgroundColor = "lightgreen";
+    element_blog.textContent = "Events"
+  } else {
+    // Element does not exist
+    console.warn("Element not found. Check your selector or ensure the element is in the DOM.");
+  }
