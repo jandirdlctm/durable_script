bf9269602a28bd42b74b6d01d2c849621c184f9b
Jandir Porta <jandirporta@Jandirs-MacBook-Pro.local>
2025-02-06 20:19:52 -0700

    changing the background color of the next two services images

--- app.js
+++ app.js
@@ -67,3 +67,19 @@ if (services_image_1) {
   // Element does not exist
   console.warn("Servuces Element1 not found. Check your selector or ensure the element is in the DOM.");
 }
+
+const services_image_2 = document.querySelector("#\\36 76dbb0e53a30563c988dbc3 > div.relative.z-10.container.mx-auto.pt-12.lg\\:pt-20.pb-12.lg\\:pb-20 > div > div > div:nth-child(2) > div > div > div > div.flex-shrink-0.relative.overflow-hidden.aspect-w-1.aspect-h-1.rounded-2xl.md\\:rounded-3xl.lg\\:rounded-4xl > span > img")
+if (services_image_2) {
+  console.log("Element exists! We can manipulate it safely.");
+  services_image_2.style.backgroundColor = "Black";
+} else {
+  console.warn("Servuces Element2 not found. Check your selector or ensure the element is in the DOM.");
+}
+
+const services_image_3 = document.querySelector("#\\36 76dbb0e53a30563c988dbc3 > div.relative.z-10.container.mx-auto.pt-12.lg\\:pt-20.pb-12.lg\\:pb-20 > div > div > div:nth-child(3) > div > div > div > div.flex-shrink-0.relative.overflow-hidden.aspect-w-1.aspect-h-1.rounded-2xl.md\\:rounded-3xl.lg\\:rounded-4xl > span > img")
+if (services_image_3) {
+  console.log("Element exists! We can manipulate it safely.");
+  services_image_3.style.backgroundColor = "Black";
+} else {
+  console.warn("Servuces Element3 not found. Check your selector or ensure the element is in the DOM.");
+}
