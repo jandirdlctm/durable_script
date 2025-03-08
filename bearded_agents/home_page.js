0f8e357618a32b5a4fb2b2fd422e8fd1af65c016
Jandir Porta <jandirporta@Jandirs-MacBook-Pro.local>
2025-03-08 11:22:35 -0700

    removed unnecessary code

--- bearded_agents/home_page.js
+++ bearded_agents/home_page.js
@@ -22,13 +22,6 @@ if (element_blog) {
     console.warn("Element not found. Check your selector or ensure the element is in the DOM.");
   }
 
-  window.addEventListener("pageshow", function(event) {
-    // If true, the page is coming from the back-forward cache
-    if (event.persisted) {
-      window.location.reload();
-    }
-  });
-
 // CODE TO RELOAD PAGE EVERYTIME WE HIT THE LOGO BUTTON
 var logo_link = document.querySelector("#website-header > div > div.col-span-2.lg\\:col-span-1 > a")
 logo_link.addEventListener('click', function(event) {
@@ -51,30 +44,3 @@ if (about_us_button) {
   console.warn("Element not found. Check your selector or ensure the element is in the DOM.");
 }
 
-const services_image_1 = document.querySelector("#\\36 76dbb0e53a30563c988dbc3 > div.relative.z-10.container.mx-auto.pt-12.lg\\:pt-20.pb-12.lg\\:pb-20 > div > div > div:nth-child(1) > div > div > div > div.flex-shrink-0.relative.overflow-hidden.aspect-w-1.aspect-h-1.rounded-2xl.md\\:rounded-3xl.lg\\:rounded-4xl > span > img")
-if (services_image_1) {
-  // Element was found
-  console.log("Element exists! We can manipulate it safely.");
-//   new_element.style.backgroundColor = "lightgreen";
-  // new_element.classList.remove('flex-col');
-  services_image_1.style.backgroundColor = "Black";
-} else {
-  // Element does not exist
-  console.warn("Servuces Element1 not found. Check your selector or ensure the element is in the DOM.");
-}
-
-const services_image_2 = document.querySelector("#\\36 76dbb0e53a30563c988dbc3 > div.relative.z-10.container.mx-auto.pt-12.lg\\:pt-20.pb-12.lg\\:pb-20 > div > div > div:nth-child(2) > div > div > div > div.flex-shrink-0.relative.overflow-hidden.aspect-w-1.aspect-h-1.rounded-2xl.md\\:rounded-3xl.lg\\:rounded-4xl > span > img")
-if (services_image_2) {
-  console.log("Element exists! We can manipulate it safely.");
-  services_image_2.style.backgroundColor = "Black";
-} else {
-  console.warn("Servuces Element2 not found. Check your selector or ensure the element is in the DOM.");
-}
-
-const services_image_3 = document.querySelector("#\\36 76dbb0e53a30563c988dbc3 > div.relative.z-10.container.mx-auto.pt-12.lg\\:pt-20.pb-12.lg\\:pb-20 > div > div > div:nth-child(3) > div > div > div > div.flex-shrink-0.relative.overflow-hidden.aspect-w-1.aspect-h-1.rounded-2xl.md\\:rounded-3xl.lg\\:rounded-4xl > span > img")
-if (services_image_3) {
-  console.log("Element exists! We can manipulate it safely.");
-  services_image_3.style.backgroundColor = "Black";
-} else {
-  console.warn("Servuces Element3 not found. Check your selector or ensure the element is in the DOM.");
-}
