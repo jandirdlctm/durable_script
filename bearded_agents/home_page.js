66792900326e65615d00ddefebd58e591da68277
Jandir Porta <jandirporta@Jandirs-MacBook-Pro.local>
2025-03-08 11:34:43 -0700

    added comments

--- bearded_agents/home_page.js
+++ bearded_agents/home_page.js
@@ -1,4 +1,5 @@
 
+// HERO SECTION BUTTONS TO BE NEXT TO EACH OTHER ON MOBILE
 const new_element = document.querySelector("#\\36 7668b4cb39ca82afeff4e37 > div > div.relative.z-10.container.mx-auto.px-5.md\\:px-6.transition-all.duration-300.pt-20.lg\\:pt-48.pb-20.lg\\:pb-48 > div > div > div.flex.md\\:inline-flex.flex-col.md\\:flex-row.gap-4.w-full.md\\:w-max.mt-4")
 if (new_element) {
   // Element was found
@@ -10,7 +11,7 @@ if (new_element) {
   // Element does not exist
   console.warn("Element not found. Check your selector or ensure the element is in the DOM.");
 }
-
+// CHANGING TEXT OF EVENTS 
 const element_blog = document.querySelector("#\\36 7693ccfa6bc38850f4d75bd > div.relative.z-10.container.mx-auto.pt-12.lg\\:pt-20.pb-12.lg\\:pb-20 > div.transition-all.transition-all.ease-in-out.duration-750.opacity-100.translate-y-0.flex.flex-col.gap-4.mb-10.items-center.text-center.mx-auto > h2")
 if (element_blog) {
     // Element was found
@@ -32,6 +33,7 @@ event.preventDefault();
 window.location.href = 'https://thebeardedagents.com';
 });
 
+// MAKING THE ABOUT US BUTTON SMALLER ON MOBILE
 const about_us_button = document.querySelector("#\\36 766912daa11ffe6a93f4745 > div.relative.z-10.container.mx-auto.pt-8.lg\\:pt-12.pb-8.lg\\:pb-12 > div > div.flex-1.flex.flex-col.max-w-240.items-start > a")
 if (about_us_button) {
   // Element was found
