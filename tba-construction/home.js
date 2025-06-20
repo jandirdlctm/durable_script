da4e00bd92abb74ac63a20a8fd0cdbd0a77a9779
Jandir Porta <jandirporta@Jandirs-MacBook-Pro.local>
2025-06-20 08:04:39 -0600

    testing again

--- tba-construction/home.js
+++ tba-construction/home.js
@@ -1,5 +1,6 @@
 // HERO SECTION BUTTONS TO BE NEXT TO EACH OTHER ON MOBILE
 const new_element = document.querySelector("#\\36 8235520296cd92ac3b63ac9 > div > div.flex-1.relative > div.transition-all.transition-all.ease-in-out.duration-500.opacity-100.translate-y-0.relative.z-10.w-full.h-full > div > div > div")
+                    
 if (new_element) {
   // Element was found
   console.log("Element exists! We can manipulate it safely.");
